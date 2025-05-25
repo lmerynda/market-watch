7cddf0a2f194ba8ff837f32c42f55987f0b22af7
lmerynda <lmerynda@gmail.com>
2025-05-25 00:28:11 -0500

    init

---
+++ src/main/java/com/lmerynda/market_watch/MarketWatchApplication.java
@@ -0,0 +1,13 @@
+package com.lmerynda.market_watch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketWatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketWatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lmerynda/market_watch/MarketWatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lmerynda.market_watch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketWatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
